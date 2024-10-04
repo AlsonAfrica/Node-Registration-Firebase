6a6426b848735462709544257f8200d2cf5bc829
Nhlakanipho <nhalakaniphoradebe337@gmail.com>
2024-10-04 12:21:49 +0200

    Update: service keys hidden

--- registration-app/src/main.jsx
+++ registration-app/src/main.jsx
@@ -1,7 +1,6 @@
 // src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { EmployeeProvider } from './contexts/EmployeeContext';  // Correct path to EmployeeContext
 import Loginpage from './Pages/Loginpage.jsx';
